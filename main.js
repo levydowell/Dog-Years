d3546330997f15ae8423442d1e4a2c6ac06226e4
levydowell <33126924+levydowell@users.noreply.github.com>
2023-11-19 11:14:27 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var myAge = 32;
+var earlyYears = 2;
+
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+laterYears *= 4;
+
+// convert actual age to dog years. 
+myAgeInDogYears = earlyYears + laterYears;
+
+//print lower case name to console with dog age and regular human age.
+myName = "Levy".toLowerCase()
+console.log(`My name is ${myName}, I am ${myAge} years old in human which is ${myAgeInDogYears}
+years old in dog years.`)
